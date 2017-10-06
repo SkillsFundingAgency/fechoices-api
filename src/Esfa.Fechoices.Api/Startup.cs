c84f1ed4fe0c036ad86c97a43784f5697b2ec182
Scott Cowan <info@scottcowan.org>
2017-10-06 11:16:19 +0100

    show the dev error page on the site

--- src/Esfa.Fechoices.Api/Startup.cs
+++ src/Esfa.Fechoices.Api/Startup.cs
@@ -46,9 +46,9 @@ namespace Esfa.Fechoices.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseDeveloperExceptionPage();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 loggerFactory.AddConsole(Configuration.GetSection("Logging"));
                 loggerFactory.AddDebug();
             }
